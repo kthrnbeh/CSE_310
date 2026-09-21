007b0e4f66c21f68510f94ff0d3edd00fd204613
kthrnbeh <kthrnbeh@yahoo.com>
2026-09-21 12:35:06 -0600

    update

--- week2/Practice.java
+++ week2/Practice.java
@@ -22,6 +22,10 @@ public class Practice {
         System.out.println(x+y+z);
         final int myNum= 10; //the nunmer can't be changed because it is final
         System.out.println(myNum);
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
     }
 
 }
