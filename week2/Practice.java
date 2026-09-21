e73e6074018a20af8a3ad9b74d4f8dcc9b09faae
kthrnbeh <kthrnbeh@yahoo.com>
2026-09-21 12:30:42 -0600

    practice

--- week2/Practice.java
+++ week2/Practice.java
@@ -3,7 +3,25 @@ public class Practice {
         System.out.println("Hello world");
         System.out.println("practicing java");
         System.out.print("so I learn what to do for my game");
-
+        System.out.println(4);    
+        System.out.println(4 + 4);
+        System.out.println(4 * 4);
+        //This is how you make a comment 
+        /* This is a multi-line comment */
+        String name="Ken";
+        int age= 15;
+        float height= 5.9f;
+        char grade= 'A';
+        boolean isMale= true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(isMale);
+        int x= 5,y= 10,z= 15;
+        System.out.println(x+y+z);
+        final int myNum= 10; //the nunmer can't be changed because it is final
+        System.out.println(myNum);
     }
 
 }
