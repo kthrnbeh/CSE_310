fa851eeacf909e7d771e16216471556ab6a43220
kthrnbeh <kthrnbeh@yahoo.com>
2026-09-21 12:15:20 -0600

    update practicing

---
+++ week2/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("practicing java");
+        System.out.print("so I learn what to do for my game");
+
+    }
+
+}
